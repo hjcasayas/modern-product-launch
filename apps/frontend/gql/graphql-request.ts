ad0b993c318019fc38ca9e40c986e29809695f77
hjcasayas <henlyjade.casayas@gmail.com>
2025-06-20 03:48:02 +0000

    fix: notfound is not allowed in root layout

--- apps/frontend/gql/graphql-request.ts
+++ apps/frontend/gql/graphql-request.ts
@@ -1,24 +1,14 @@
 "use server";
 
 import request from "graphql-request";
-import { notFound } from "next/navigation";
 
 export const graphqlRequest = async <TData = unknown, TVariables = never>(
   query: string,
   variables?: TVariables
 ): Promise<TData> => {
-  try {
-    const response = await request(
-      process.env.STRAPI_GRAPHQL_ENDPOINT!,
-      query,
-      variables ?? {}
-    );
-    if (response == null) {
-      notFound();
-    }
-
-    return response as TData;
-  } catch (error) {
-    notFound();
-  }
+  return await request(
+    process.env.STRAPI_GRAPHQL_ENDPOINT!,
+    query,
+    variables ?? {}
+  );
 };
