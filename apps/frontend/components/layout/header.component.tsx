3b8b6b8340169f51662316b3d9e4eeaf81899b65
hjcasayas <henlyjade.casayas@gmail.com>
2025-06-20 03:12:50 +0000

    refactor: websitename in header component

--- apps/frontend/components/layout/header.component.tsx
+++ apps/frontend/components/layout/header.component.tsx
@@ -1,6 +1,5 @@
 import { LayoutHeader } from "@/interfaces/layout/header.interface";
 
-import { WebsiteNameComponent } from "../elements/website-name.component";
 import { MobileMenuComponent } from "./mobile-menu.component";
 
 import { MenuComponent } from "./menu-component";
@@ -14,7 +13,7 @@ export const HeaderComponent = ({
     <header>
       <div className="px-5 pb-8 pt-5 md:px-10">
         <div className="relative flex items-start justify-between pb-12 md:items-center">
-          <WebsiteNameComponent name={websiteName.name} />
+          <h1 className="text-3xl font-medium">{websiteName}</h1>
           <MobileMenuComponent cta={cta} navigation={navigation} />
           <MenuComponent cta={cta} navigation={navigation} />
         </div>
