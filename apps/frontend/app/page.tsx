9b9ae14a158b229f879f0ec48d219fbeffd17c50
hjcasayas <henlyjade.casayas@gmail.com>
2025-06-20 14:07:36 +0000

    feat(frontend): use block renderer in root page with the landing page single type

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,3 +1,12 @@
-export default function HomePage() {
-  return <p>home-page works!</p>;
+import { BlockRenderer } from "@/components/blocks-renderer";
+import { LandingPageSingle } from "@/components/single/landing-page/landing-page.interface";
+import { landingPageQuery } from "@/components/single/landing-page/landing-page.query";
+import { graphqlRequest } from "@/gql/graphql-request";
+
+export default async function HomePage() {
+  const { landingPage } = await graphqlRequest<{
+    landingPage: LandingPageSingle;
+  }>(landingPageQuery);
+
+  return <BlockRenderer {...landingPage} />;
 }
