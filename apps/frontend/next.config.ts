72981a62ad90332675f98294397e6016a3992ab7
hjcasayas <henlyjade.casayas@gmail.com>
2025-06-20 14:00:03 +0000

    feat(frontend): fix issue on loading images

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "cms",
+        port: "1337",
+        pathname: "/uploads/**/*",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**/*",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
