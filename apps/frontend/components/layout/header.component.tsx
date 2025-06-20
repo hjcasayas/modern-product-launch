6b0c13b3a0e1c05a024be90ff12550142a7610ba
hjcasayas <henlyjade.casayas@gmail.com>
2025-06-20 14:04:00 +0000

    refactor(frontend): use h2 instead of h1 for the website name

--- apps/frontend/components/layout/header.component.tsx
+++ apps/frontend/components/layout/header.component.tsx
@@ -13,9 +13,9 @@ export const HeaderComponent = ({
     <header>
       <div className="md:px-10 md:pb-8 md:pt-5">
         <div className="rounded-4xl relative flex items-start justify-between pb-12 shadow-2xl md:items-center md:rounded-none md:shadow-none">
-          <h1 className="pl-5 pt-5 text-4xl font-medium md:pl-0 md:pt-0">
+          <h2 className="pl-5 pt-5 text-4xl font-medium md:pl-0 md:pt-0">
             {websiteName}
-          </h1>
+          </h2>
           <MobileMenuComponent cta={cta} navigation={navigation} />
           <MenuComponent cta={cta} navigation={navigation} />
         </div>
