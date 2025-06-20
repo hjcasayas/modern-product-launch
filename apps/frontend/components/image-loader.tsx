c63517350fbd4873cf7f4ad7be55072cd7f52898
hjcasayas <henlyjade.casayas@gmail.com>
2025-06-20 14:01:06 +0000

    feat(frontend): added image loader component

---
+++ apps/frontend/components/image-loader.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Image, { ImageProps } from "next/image";
+
+import { imageLoaderProp } from "@/utils/image-loader.util";
+import { getStrapiImage } from "@/utils/get-strapi-image.util";
+
+export interface ImageLoaderProps extends ImageProps {
+  classname?: string;
+}
+
+export const ImageLoader = ({ className, ...linkProps }: ImageLoaderProps) => {
+  return (
+    <Image
+      {...linkProps}
+      src={getStrapiImage(linkProps.src)}
+      className={`relative mx-auto block ${className}`}
+      loader={imageLoaderProp}
+    />
+  );
+};
