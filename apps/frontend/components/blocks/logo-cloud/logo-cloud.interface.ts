f24e27a867441486470f04094704f619dc0862bf
hjcasayas <henlyjade.casayas@gmail.com>
2025-06-23 23:19:58 +0000

    fix(fe): lint issue

--- apps/frontend/components/blocks/logo-cloud/logo-cloud.interface.ts
+++ apps/frontend/components/blocks/logo-cloud/logo-cloud.interface.ts
@@ -1,5 +1,3 @@
-import { StrapiBase } from "@/interfaces/strapi/base.interface";
-import { StrapiBlock } from "@/interfaces/strapi/block.interface";
 import { StrapiComponentBase } from "@/interfaces/strapi/component-base.interface";
 import { StrapiImage } from "@/interfaces/strapi/image.interface";
 
