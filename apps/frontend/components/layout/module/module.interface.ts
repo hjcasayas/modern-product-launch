c72f4eec769090866c4acb7f030e13ca459869f5
hjcasayas <henlyjade.casayas@gmail.com>
2025-06-23 14:35:26 +0000

    refactor: remove id from module layout interface

--- apps/frontend/components/layout/module/module.interface.ts
+++ apps/frontend/components/layout/module/module.interface.ts
@@ -1,8 +1,6 @@
-import { StrapiBase } from "@/interfaces/strapi/base.interface";
 import { StrapiImage } from "@/interfaces/strapi/image.interface";
 
-export interface ModuleLayout extends StrapiBase {
-  id: number;
+export interface ModuleLayout {
   title: string;
   icon: StrapiImage;
   description: string;
